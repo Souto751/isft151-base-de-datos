216ec49b623013bec3c7f2d9b66ea734e193be19
Agustin Souto <agus.souto75@gmail.com>
2021-06-03 16:34:45 -0300

    Carpetas movidas dentro de Python

--- Files/puntajes_03/puntajes_03.py
+++
@@ -1,6 +0,0 @@
-import puntajes_csv    
-
-valores = [("Pepe", 108, "4:16"), ("Juana", 2315, "8:42")]
-puntajes_csv.guardar_puntajes("puntajes.txt", valores)
-recuperado = puntajes_csv.recuperar_puntajes("puntajes.txt")
-print(recuperado)
